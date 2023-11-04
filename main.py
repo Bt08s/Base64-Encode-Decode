813431ef63d8b17df32588e2f96acbb5c9d81501
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-04 23:53:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def decode_text(text, count):
     return decoded_text.decode('utf-8')
 
 
-with dpg.window(tag="Primary Window"):
+with dpg.window(tag="Coder window"):
     def encode(sender):
         print(sender)
         text = dpg.get_value("text")
@@ -49,18 +49,18 @@ with dpg.window(tag="Primary Window"):
 
     dpg.add_input_text(label="Text", tag="text")
     with dpg.popup(dpg.last_item()):
-        dpg.add_text("Text to encode or decode.")
+        dpg.add_text("Text to encode or decode")
     dpg.add_input_text(label="Count", tag="count")
     with dpg.popup(dpg.last_item()):
-        dpg.add_text("Number of times to encode or decode.")
+        dpg.add_text("Number of times to encode or decode")
 
-    dpg.add_button(label="Encode", callback=encode, width=314)
-    dpg.add_button(label="Decode", callback=decode, width=314)
     dpg.add_input_text(label="Output", tag="output", readonly=True, multiline=True)
+    dpg.add_button(label="Encode", callback=encode, width=100)
+    dpg.add_button(label="Decode", callback=decode, width=100)
 
 dpg.create_viewport(title='Advanced Base64 UTF-8 encoder/decoder by Bt08s', width=500, height=300)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
+dpg.set_primary_window("Coder window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
