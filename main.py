547e18e3623058dd303531160ae52c01d963f95b
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-06 18:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,10 +63,10 @@ with dpg.window(tag="Coder window"):
 
 
     dpg.add_input_text(label="Text", tag="text")
-    with dpg.popup(dpg.last_item()):
+    with dpg.tooltip(dpg.last_item()):
         dpg.add_text("Text to process")
     dpg.add_input_text(label="Count", tag="count")
-    with dpg.popup(dpg.last_item()):
+    with dpg.tooltip(dpg.last_item()):
         dpg.add_text("Number of times to process")
 
     dpg.add_input_text(label="Output", tag="output", readonly=True)
