1c92cf72b668122a9638eac756394f2dde9dc973
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-06 17:45:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+import clipboard
 import base64
 
 dpg.create_context()
@@ -13,7 +14,11 @@ def encode_text(text, count):
     # Encode the text count times
     encoded_text = text.encode('utf-8')
     for _ in range(count):
-        encoded_text = base64.b64encode(encoded_text)
+        try:
+            encoded_text = base64.b64encode(encoded_text)
+        except:
+            print("Error: Unable to decode the text. The input may not be valid Base64.")
+            return None
 
     return encoded_text.decode('utf-8')
 
@@ -29,11 +34,17 @@ def decode_text(text, count):
     for _ in range(count):
         try:
             decoded_text = base64.b64decode(decoded_text)
-        except base64.binascii.Error:
+        except:
             print("Error: Unable to decode the text. The input may not be valid Base64.")
             return None
 
     return decoded_text.decode('utf-8')
+
+
+def clear_values():
+    dpg.set_value("text", "")
+    dpg.set_value("count", "")
+    dpg.set_value("output", "")
 
 
 with dpg.window(tag="Coder window"):
@@ -43,11 +54,13 @@ with dpg.window(tag="Coder window"):
         encoded_text = encode_text(text, count)
         dpg.set_value("output", encoded_text)
 
+
     def decode():
         text = dpg.get_value("text")
         count = int(dpg.get_value("count"))
         decoded_text = decode_text(text, count)
         dpg.set_value("output", decoded_text)
+
 
     dpg.add_input_text(label="Text", tag="text")
     with dpg.popup(dpg.last_item()):
@@ -56,10 +69,26 @@ with dpg.window(tag="Coder window"):
     with dpg.popup(dpg.last_item()):
         dpg.add_text("Number of times to process")
 
-    dpg.add_input_text(label="Output", tag="output", readonly=True, multiline=True)
+    dpg.add_input_text(label="Output", tag="output", readonly=True)
+    dpg.add_spacing(count=3)
     dpg.add_button(label="Encode", callback=encode, width=100)
     dpg.add_button(label="Decode", callback=decode, width=100)
+    dpg.add_button(label="Copy", callback=lambda: clipboard.copy(dpg.get_value("output")), width=100)
+    dpg.add_button(label="Clear", callback=clear_values, width=100)
 
+with dpg.theme() as global_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_style(dpg.mvStyleVar_WindowRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_GrabRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_TabRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_ChildRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_PopupRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_ScrollbarRounding, 3)
+        dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 4, 4)
+        dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 5, 5)
+
+dpg.bind_theme(global_theme)
 dpg.create_viewport(title='Advanced Base64 UTF-8 encoder/decoder by Bt08s', width=500, height=300)
 dpg.setup_dearpygui()
 dpg.show_viewport()
